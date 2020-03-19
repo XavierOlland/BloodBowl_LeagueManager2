a08526516f3177e1162ab5081ad07e5e3f90a940
Xavier Olland <xavier.olland@gmail.com>
2020-03-19 18:56:46 +0200

    8 - Champion component refactoring and use

--- src/components/Champion.vue
+++ src/components/Champion.vue
@@ -1,38 +1,78 @@
 <template>
   <div id="Champion" class="plain prime">
-    <h3>CHAMPION</h3>
-    <h4>Joute de Bretonnie 2518</h4>
-    <hr>
-    <h1 class="zelda">Les Busards du Blizzard</h1>
-    <hr>
-    <h2 ng-style="colours[0].color">Cysterion</h2>
-    <!--div class="button">
-      la compétition
-    </div-->
+    <Helmet class="helmet" :race="race" :logo="logo" :colours="colours" />
+    <div class="spacer">
+      <h3 v-if="mode!='list'">CHAMPION</h3>
+      <h3>{{competition.name}}</h3>
+      <hr>
+      <h1>{{team}}</h1>
+      <hr>
+      <h2>{{coach}}</h2>
+    </div>
+    <div class="info">
+      <h3 v-if="mode!='list'">CHAMPION</h3>
+      <h3>{{competition.name}}</h3>
+      <hr>
+      <h1>{{team}}</h1>
+      <hr>
+      <h2>{{coach}}</h2>
+    </div>
+    <Button class="button" :id="'Prime'" :text="'La Compétition'" @clicked="$router.push({ name: 'Competition', params: { id: competition.id }})" />
   </div>
 </template>
 
 <script>
+  import Button from '../components/ui/Button.vue';
+  import Helmet from '../components/ui/Helmet.vue';
 
-export default {
-  name: 'Champion',
-  props: {},
-  data() {
-    return {
-      champion:{}
+  export default {
+    name: 'Champion',
+    components: {
+      Button,
+      Helmet
+    },
+    props: {
+      mode: String,
+      competition: Object,
+      coach: String,
+      team: String,
+      race: Number,
+      logo: String,
+      colours: Array
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
-  #Champion {
-    text-align: center;
-  }
-  hr {
-    width: 80%;
-    border-top: 1px solid #ffffff;
-    text-align: center;
-    margin: 10px auto;
-  }
+    #Champion {
+      text-align: center;
+      margin-top: 50px
+    }
+    hr {
+      width: 80%;
+      border-top: 1px solid #ffffff;
+      text-align: center;
+      margin: 10px auto;
+    }
+    .helmet {
+      position: absolute;
+      right:-75px;
+      top:-50px;
+      z-index:0;
+      width:200px;
+      height:200px;
+      text-align: left !important;
+    }
+    .spacer {
+      opacity:0;
+    }
+    .info {
+      position: absolute;
+      top:20px;
+      width:calc(100% - 30px);
+      z-index: 3
+    }
+    .button {
+      z-index:5
+    }
 </style>
--- src/views/Archives.vue
+++ src/views/Archives.vue
@@ -1,40 +1,68 @@
 <template>
-  <div id="Archives" class="view container">
+  <div id="Archives" class="view container" >
+    <div class="row">
+      <div class="col-lg-3 hidden-md-down scroll">
+        <div class="plain seconde searchBox">
+          asd
+        </div>
+      </div>
+      <div class="col-lg-9 scroll">
+        <div class="col-sm-6 col-xl-4" v-for="archive in archives" :key="archive.id">
+          <Champion v-if="archive.champion==1 && archive.standing[0]"
+            :mode="'list'"
+            :competition="{id:archive.id,name:archive.season}"
+            :coach="archive.standing[0].coach"
+            :team="archive.standing[0].name"
+            :race="Number(archive.standing[0].race)"
+            :logo="archive.standing[0].logo"
+            :colours="[archive.standing[0].color_1,archive.standing[0].color_2]"
+          />
+        </div>
 
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
-  import Button from '../components/ui/Button.vue';
+  import Champion from '../components/Champion.vue'
+
 
   export default {
     name: 'Archives',
     components: {
-      Button
+      Champion
     },
     props: {
       dictionnary: Array
     },
     data(){
       return {
-
+        isFetching: true,
       }
     },
     computed:{
-
+      archives(){
+        return this.$store.state.archives.archives;
+      }
     },
     methods: {
 
     },
     mounted() {
-
+      this.$store.dispatch('archives/fetchArchives')
+    },
+    watch: {
+      archives: function() {
+        this.isFetching = this.archives.length>0? true : false;
+      }
     }
 
   }
 </script>
 
 <style lang="scss" scoped>
-
+  .something {}
 
 </style>
