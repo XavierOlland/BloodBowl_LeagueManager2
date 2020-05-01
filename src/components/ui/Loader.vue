b8e52c829ee74303b79b9c50481e92f3c752adcd
Xavier Olland <xavier.olland@gmail.com>
2020-05-01 18:30:13 +0300

    20 - Missing loader

---
+++ src/components/ui/Loader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="loader veil">
+    <img src="~@/assets/elements/blood.png">
+    <h3>{{text}}</h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Loader',
+  props: {
+    text: String,
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .loader {
+    img {
+      width:40%;
+      animation-name: opacity;
+      animation-duration: 4s;
+      animation-iteration-count: infinite;
+    }
+    h3 {
+      position: absolute;
+    }
+  }
+  @keyframes opacity {
+    0%   {opacity: 0.1;}
+    50%  {opacity: 1;}
+    100% {opacity: 0.1;}
+  }
+</style>
