875603e9fa9b3cf0c4e2c44c6e5935c34cd91366
Xavier Olland <xavier.olland@gmail.com>
2020-05-07 14:22:56 +0300

    22 - View and routing

---
+++ src/views/Admin.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="Admin" class="view container">
+    <div class="row">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Admin',
+    components: {
+
+    },
+    data(){
+      return {
+        admin: window.admin
+      }
+    },
+    computed: {
+      user() {
+        return this.$store.state.user
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
