16d966c7f75bf43d690e9c109ba16b800555ff12
Xavier Olland <xavier.olland@gmail.com>
2020-06-17 12:46:17 +0300

    34 - sass-loader 8

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-	chainWebpack: (config) => {
-		const svgRule = config.module.rule('svg');
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
 
-		svgRule.uses.clear();
+    svgRule.uses.clear();
 
-		svgRule
-			.use('vue-svg-loader')
-			.loader('vue-svg-loader');
-	},
-	css: {
-		loaderOptions: {
-			sass: {
-				data: `
-        @import "@/assets/styles/_main.scss";
-        @import "@/assets/styles/_helmets.scss";
-        `
-			}
-		}
-	},
-	publicPath: '/'
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `
+          @import "@/assets/styles/_main.scss";
+          @import "@/assets/styles/_helmets.scss";
+          `
+      }
+    }
+  },
+  publicPath: '/'
 };
